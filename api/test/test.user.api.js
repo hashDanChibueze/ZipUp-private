6d5055adae0089572ecc5a175e49d16ab968169a
Karan Goel <karan@goel.im>
2014-03-25 15:03:40 -0700

    add more tests

--- api/test/test.user.api.js
+++ api/test/test.user.api.js
@@ -10,31 +10,31 @@ var api = 'http://localhost:3000';
 
 describe('User', function() {
 
-    describe('API signup response', function(done) {
+    var user = {
+        email: 'test1@test.com',
+        password: 'password'
+    };
 
-        var user = {
-            email: 'test1@test.com',
-            password: 'password'
-        };
+    var user2 = {
+        email: 'test1@test.com',
+        password: 'password'
+    };
 
-        var user2 = {
-            email: 'test1@test.com',
-            password: 'password'
-        };
+    before(function(done) {
+        mongoose.connect(secrets.db);
+        User.remove(done);
+    });
 
-        before(function(done) {
-            mongoose.connect(secrets.db);
-            User.remove(done);
-        });
+    afterEach(function(done) {
+        User.remove(done);
+    });
 
-        afterEach(function(done) {
-            User.remove(done);
-        });
+    after(function(done) {
+        mongoose.connection.close();
+        done();
+    });
 
-        after(function(done) {
-            mongoose.connection.close();
-            done();
-        });
+    describe('API signup', function(done) {
 
         it('should signup just fine and return status 200.', function(done) {
             request(api)
@@ -57,6 +57,155 @@ describe('User', function() {
                         .end(function(err, res) {
                             res.should.have.status(400);
                             res.body.should.have.property('errors', 'User with that email already exists.');
+                            done();
+                        });
+                });
+        });
+
+    });
+
+    describe('API signin', function(done) {
+
+        it('should sign in fine, and response should have user.', function(done) {
+            request(api)
+                .post('/signup')
+                .send(user)
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    request(api)
+                        .post('/signin')
+                        .send(user)
+                        .end(function(e, r) {
+                            r.should.have.status(200);
+                            r.body.should.have.property('user');
+                            done();
+                        });
+                });
+        });
+
+        it('should not work since user does not exist.', function(done) {
+            request(api)
+                .post('/signin')
+                .send(user)
+                .end(function(e, r) {
+                    r.should.have.status(400);
+                    r.body.should.have.property('response', 'fail');
+                    done();
+                });
+        });
+
+    });
+
+    describe('Account details', function(done) {
+
+        it('should return the user.', function(done) {
+            request(api)
+                .post('/signup')
+                .send(user)
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    var cookie = res.headers['set-cookie'];
+                    request(api)
+                        .get('/account')
+                        .set('cookie', cookie)
+                        .end(function(e, r) {
+                            r.should.have.status(200);
+                            r.body.should.have.property('user');
+                            done();
+                        });
+                });
+        });
+
+        it('should not work as user signed out.', function(done) {
+            request(api)
+                .post('/signup')
+                .send(user)
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    request(api)
+                        .get('/signout')
+                        .end(function(e, r) {
+                            request(api)
+                                .get('/account')
+                                .end(function(e, r) {
+                                    r.should.have.status(401);
+                                    done();
+                                });
+                        });
+                });
+        });
+
+    });
+
+    describe('Update profile', function(done) {
+
+        it('should update user location, name and email.', function(done) {
+            request(api)
+                .post('/signup')
+                .send(user)
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    var cookie = res.headers['set-cookie'];
+                    request(api)
+                        .post('/account/profile')
+                        .set('cookie', cookie)
+                        .send({'email': 'new@test.com',
+                                'name': 'Hello',
+                                'location': 'WA'})
+                        .end(function(e, r) {
+                            r.should.have.status(200);
+                            r.body.should.have.property('user');
+                            r.body.user.should.have.property('email', 'new@test.com');
+                            r.body.user.profile.should.have.property('name', 'Hello');
+                            r.body.user.profile.should.have.property('location', 'WA');
+                            done();
+                        });
+                });
+        });
+
+        it('should fail to update email as used by another account.', function(done) {
+            request(api)
+                .post('/signup')
+                .send(user)
+                .end(function(e, r) {
+                    r.should.have.status(200);
+                    request(api)
+                        .post('/signup')
+                        .send({'email': 'test2@test.com', 'password': 'password'})
+                        .end(function(err, res) {
+                            res.should.have.status(200);
+                            var cookie = res.headers['set-cookie'];
+                            request(api)
+                                .post('/account/profile')
+                                .set('cookie', cookie)
+                                .send({'email': user.email})
+                                .end(function(e2, r2) {
+                                    r2.should.have.status(400);
+                                    r2.body.should.have.property('errors', 'Email is associated with another user.');
+                                    done();
+                                });
+                        });
+                });
+        });
+
+    });
+
+    describe('Update password', function(done) {
+
+        it('should update password.', function(done) {
+            request(api)
+                .post('/signup')
+                .send(user)
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    var cookie = res.headers['set-cookie'];
+                    request(api)
+                        .post('/account/password')
+                        .set('cookie', cookie)
+                        .send({'password': 'newPassword'})
+                        .end(function(e, r) {
+                            r.should.have.status(200);
+                            r.body.should.have.property('response', 'ok');
                             done();
                         });
                 });
