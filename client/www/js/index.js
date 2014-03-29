b5991cb1873307a918f98bd5de81b44c5f11927c
Karan Goel <karan@goel.im>
2014-03-28 18:59:44 -0700

    many changes, styling, trying to make maps work

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -1,33 +1,38 @@
 var app = {
     server : 'http://z-api.herokuapp.com',
     //uuid : window.device.uuid,
+
     // Application Constructor
     initialize: function() {
+        console.log("initialize");
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-        $(document).ready(app.onDeviceReady);
+        console.log("bind events");
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+            console.log("on device");
+            document.addEventListener("deviceready", this.onDeviceReady, true);
+        } else {
+            console.log("on desktop");
+            $(document).ready(this.onDeviceReady);
+        }
     },
+
     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        
+        console.log("on deviceready");
+        navigator.geolocation.getCurrentPosition(showOnMap);
     }
 };
 
 var showOnMap = function(position) {
+    console.log("showing map");
     var pinColor = "EEEEEE";
     var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
         new google.maps.Size(21, 34),
@@ -57,50 +62,50 @@ var showOnMap = function(position) {
     });
 };
 
-ko.bindingHandlers.map = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        console.log("in the binding");
-        navigator.geolocation.getCurrentPosition(showOnMap);
+// ko.bindingHandlers.map = {
+//     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+//         console.log("in the binding");
+//         //navigator.geolocation.getCurrentPosition(showOnMap);
 
-        // var mapObj = ko.utils.unwrapObservable(valueAccessor());
-        // console.log(mapObj);
-        // var latLng = new google.maps.LatLng(
-        //     ko.utils.unwrapObservable(mapObj.lat),
-        //     ko.utils.unwrapObservable(mapObj.lng));
-        // var mapOptions = { center: latLng,
-        //                   zoom: 17, 
-        //                   mapTypeId: google.maps.MapTypeId.ROADMAP};
+//         // var mapObj = ko.utils.unwrapObservable(valueAccessor());
+//         // console.log(mapObj);
+//         // var latLng = new google.maps.LatLng(
+//         //     ko.utils.unwrapObservable(mapObj.lat),
+//         //     ko.utils.unwrapObservable(mapObj.lng));
+//         // var mapOptions = { center: latLng,
+//         //                   zoom: 17, 
+//         //                   mapTypeId: google.maps.MapTypeId.ROADMAP};
         
-        // mapObj.googleMap = new google.maps.Map(element, mapOptions);
+//         // mapObj.googleMap = new google.maps.Map(element, mapOptions);
         
-        // mapObj.marker = new google.maps.Marker({
-        //     map: mapObj.googleMap,
-        //     position: latLng,
-        //     title: "You Are Here",
-        //     draggable: true
-        // });     
+//         // mapObj.marker = new google.maps.Marker({
+//         //     map: mapObj.googleMap,
+//         //     position: latLng,
+//         //     title: "You Are Here",
+//         //     draggable: true
+//         // });     
         
-        // mapObj.onChangedCoord = function(newValue) {
-        //     var latLng = new google.maps.LatLng(
-        //         ko.utils.unwrapObservable(mapObj.lat),
-        //         ko.utils.unwrapObservable(mapObj.lng));
-        //     mapObj.googleMap.setCenter(latLng);                 
-        // };
+//         // mapObj.onChangedCoord = function(newValue) {
+//         //     var latLng = new google.maps.LatLng(
+//         //         ko.utils.unwrapObservable(mapObj.lat),
+//         //         ko.utils.unwrapObservable(mapObj.lng));
+//         //     mapObj.googleMap.setCenter(latLng);                 
+//         // };
         
-        // mapObj.onMarkerMoved = function(dragEnd) {
-        //     var latLng = mapObj.marker.getPosition();
-        //     mapObj.lat(latLng.lat());
-        //     mapObj.lng(latLng.lng());
-        // };
+//         // mapObj.onMarkerMoved = function(dragEnd) {
+//         //     var latLng = mapObj.marker.getPosition();
+//         //     mapObj.lat(latLng.lat());
+//         //     mapObj.lng(latLng.lng());
+//         // };
         
-        // mapObj.lat.subscribe(mapObj.onChangedCoord);
-        // mapObj.lng.subscribe(mapObj.onChangedCoord);  
+//         // mapObj.lat.subscribe(mapObj.onChangedCoord);
+//         // mapObj.lng.subscribe(mapObj.onChangedCoord);  
         
-        // google.maps.event.addListener(mapObj.marker, 'dragend', mapObj.onMarkerMoved);
+//         // google.maps.event.addListener(mapObj.marker, 'dragend', mapObj.onMarkerMoved);
         
-        // $("#" + element.getAttribute("id")).data("mapObj",mapObj);
-    }
-};
+//         // $("#" + element.getAttribute("id")).data("mapObj",mapObj);
+//     }
+// };
 var viewModel = new ViewModel();
 
 $(document).ready(function () {
